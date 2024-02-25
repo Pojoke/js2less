9ee168198a24e063924cef73a0a88635879fc271
Pojoke <pavlykdmutro@gmail.com>
2024-02-25 11:14:36 +0200

    first commit

---
+++ js1.js
@@ -0,0 +1,11 @@
+let x = prompt("Input x");
+let y = prompt("Input y");
+let mult = x * y;
+// alert(x * y);
+// if (isNaN(mult));
+if (isNaN(mult)) {
+    alert("Error!")
+}
+else {
+    alert(mult);
+}
