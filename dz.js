087cf9c613a77639ac3016a6bfbd717c95cff207
Pojoke <pavlykdmutro@gmail.com>
2024-03-09 20:19:01 +0200

    day

--- dz.js
+++ dz.js
@@ -2,10 +2,11 @@ let day = prompt("Input day");
 if (day >= 1 && day <= 5) {
     alert(" Не вихідний ");
 }
-else {
-    (day >= 6 && day <= 7);
+else if (day >= 6 && day <= 7){
     alert("  Вихідний ");
-
+}
+else{
+    alert("  Помилка ");
 }
 
 
