536e2e0f9acdc28903a146ba10097c2dfd59b5e5
Pojoke <pavlykdmutro@gmail.com>
2024-03-09 09:14:55 +0200

    porarocu

---
+++ poraroc.js
@@ -0,0 +1,15 @@
+let n = prompt("Перевірити пору року");
+let s;
+
+
+switch (n) {
+    case "Зима": s = alert("Холодно"); break;
+    case "Літо": s = alert("Спекотно"); break;
+    case "Осінь": s = alert("Прохолодно"); break;
+    case "Весна": s = alert("Тепло"); break;
+
+
+
+
+}
+document.write(s);
